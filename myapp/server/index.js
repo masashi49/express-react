4b460a77ac4af721f79f9d20ff395de4f73bb173
山田将史 <13416062+masashi49@users.noreply.github.com>
2020-01-01 23:54:27 +0900

    .scssを使用する

--- myapp/server/index.js
+++ myapp/server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path"); // NEW
 
-const app = express();
+const app = express(); //読み込んだexpressモジュールをinstans化する。
 const port = process.env.PORT || 3000;
 const DIST_DIR = path.join(__dirname, "../dist"); // NEW
 const HTML_FILE = path.join(DIST_DIR, "index.html"); // NEW
@@ -9,13 +9,18 @@ const mockResponse = {
   foo: "bar",
   bar: "foo"
 };
-app.use(express.static(DIST_DIR)); // NEW
+app.use(express.static(DIST_DIR)); // DIST_DIRのパスにある静的ファイルを提供する
+
 app.get("/api", (req, res) => {
+  console.log(req);
   res.send(mockResponse);
 });
+
 app.get("/", (req, res) => {
+  //expressを使って静的ページを出力する
   res.sendFile(HTML_FILE); // EDIT
 });
+
 app.listen(port, function() {
   console.log("App listening on port: " + port);
 });
